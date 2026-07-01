4736b38b3139fff1f845b74c07eba3273c22d55d
Lucas Prett Campagna <lucasprett129@gmail.com>
2026-07-01 18:06:44 -0300

    Fix base path for S3 deploy (serve from /)

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,24 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-const dev = process.argv.includes('dev');
+const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: true,
-			strict: false
-		}),
-		paths: {
-			base: dev ? '' : '/calculadora-financeira'
-		}
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: "index.html",
+      precompress: true,
+      strict: false,
+    }),
+    paths: {
+      base:
+        process.env.SVELTE_BASE_PATH ?? (dev ? "" : "/calculadora-financeira"),
+    },
+  },
 };
 
 export default config;
